ac41540b029a6ed3976b1ebf9e85c36dab56bd8b
Abd <abdullahashfaq997@gmail.com>
2022-02-14 17:02:34 +0500

    added project intial

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+
+const Topbar = () => {
+	return <div></div>;
+};
+
+export default Topbar;
